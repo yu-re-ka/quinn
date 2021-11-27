22065b1515f312b7f7bb86269bfa1df4b5c04c01
Benjamin Saunders <ben.e.saunders@gmail.com>
2021-11-26 18:31:01 -0800

    Style tweak

--- quinn-proto/src/endpoint.rs
+++ quinn-proto/src/endpoint.rs
@@ -204,13 +204,10 @@ impl Endpoint {
         //
 
         let dst_cid = first_decode.dst_cid();
-        let known_ch = {
-            let ch = if self.local_cid_generator.cid_len() > 0 {
-                self.connection_ids.get(&dst_cid)
-            } else {
-                None
-            };
-            ch.or_else(|| {
+        let known_ch = (self.local_cid_generator.cid_len() > 0)
+            .then(|| self.connection_ids.get(&dst_cid))
+            .flatten()
+            .or_else(|| {
                 if first_decode.is_initial() || first_decode.is_0rtt() {
                     self.connection_ids_initial.get(&dst_cid)
                 } else {
@@ -232,8 +229,7 @@ impl Endpoint {
                 self.connection_reset_tokens
                     .get(remote, &data[data.len() - RESET_TOKEN_SIZE..])
             })
-            .cloned()
-        };
+            .cloned();
         if let Some(ch) = known_ch {
             return Some((
                 ch,
